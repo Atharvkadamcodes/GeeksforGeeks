45666144e52d358074e235c668b300a84874a272
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2026-08-26 22:01:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalinSent(String s) {
+        // code here
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sb.append(ch);
+            }
+        }
+
+        int left = 0, right = sb.length() - 1;
+        while (left < right) {
+            if (sb.charAt(left) != sb.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
