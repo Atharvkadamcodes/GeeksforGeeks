b5da932627249ee8b1249449715434bde538faca
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2026-08-26 22:02:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Outermost Parentheses of Primitives/outermost-parentheses-of-primitives.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String removeOuter(String s) {
+        // code here
+        int depth = 0;
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                if (depth > 0) {
+                    str.append(ch);
+                }
+                depth++;
+            } 
+            else {
+                depth--;
+                if (depth > 0) {
+                    str.append(ch);
+                }
+            }
+        }
+
+        return str.toString();
+    }
+}
